1a39edf4332f3b78f7569f7ec1a1a84ee870c901
Aaqib-Innov8_Fusion <aaqib.innov8fusion@gmail.com>
2024-06-18 18:42:52 +0530

    Add AddAuthentication,AddApiVersioning and UseSwaggerUI

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-using Microsoft.ApplicationInsights.Extensibility;
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Reflection;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
@@ -9,9 +14,6 @@ Log.Logger = new LoggerConfiguration()
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Logging.ClearProviders(); // replaced with Serilog
-//builder.Logging.AddConsole();
-
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
 if (environment == Environments.Development)
@@ -19,6 +21,7 @@ if (environment == Environments.Development)
     builder.Host.UseSerilog(
         (context, loggerConfiguration) => loggerConfiguration
             .MinimumLevel.Debug()
+            .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
             .WriteTo.Console());
 }
 else
@@ -28,24 +31,19 @@ else
             .MinimumLevel.Debug()
             .WriteTo.Console()
             .WriteTo.File("logs/task_mgr.txt", rollingInterval: RollingInterval.Day));
-            /* .WriteTo.ApplicationInsights(new TelemetryConfiguration
-            {
-                InstrumentationKey = builder.Configuration["ApplicationInsightsInstrumentationKey"]
-            }, TelemetryConverter.Traces)); */
 }
 
-// Add services to the container.
+// Add services to the DI container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
-
-/* builder.Services.AddControllers(options => // if you need to change input/output format uncomment it 
+/* builder.Services.AddControllers(options => // if you need to change input/output format only uncomment it 
 {
     options.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson()
 .AddXmlDataContractSerializerFormatters();  */
 
-builder.Services.AddProblemDetails(); // descriptive error details
+builder.Services.AddControllers().AddNewtonsoftJson();
 
+builder.Services.AddProblemDetails(); // descriptive error details
 
 builder.Services.AddDbContext<TaskDbContext>(DbContextOptions  // Regsitered with Scoped lifetime 
     => DbContextOptions.UseSqlServer( 
@@ -55,9 +53,103 @@ builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Convert.FromBase64String(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
+
+//AddAuthorization policy with RequireClaim city //I didn't apply this policy to controller
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeWiki", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("user_name", "wiki");
+    });
+
+});
+
+builder.Services.AddApiVersioning(setupAction => // Enable API versioning
+{
+    setupAction.ReportApiVersions = true;
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+
+}).AddMvc()
+.AddApiExplorer(setupAction => // add apiversions of url in Swagger
+{
+    setupAction.SubstituteApiVersionInUrl = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var apiVersionDescriptionProvider = builder.Services.BuildServiceProvider()
+                                    .GetRequiredService<IApiVersionDescriptionProvider>();
+
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    foreach (var description in
+       apiVersionDescriptionProvider.ApiVersionDescriptions)
+    {
+        setupAction.SwaggerDoc(
+            $"{description.GroupName}",
+            new()
+            {
+                Title = "Task Management API",
+                Version = description.ApiVersion.ToString(),
+                Description = "Through this API you can access tasks and details."
+            });
+    }
+
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+
+    setupAction.AddSecurityDefinition("TaskMgrApiBearerAuth", new() // Enable Authorization in Swagger Doc
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Input a valid token to access this API"
+    });
+
+    // openAppiSecurityRequirement Object - to add bearer token in OpenAPI DOC header request
+    setupAction.AddSecurityRequirement(new()
+    {
+        {
+            new ()
+            {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "TaskMgrApiBearerAuth" }
+            },
+            new List<string>()
+        }
+    });
+
+});
+
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
 
@@ -65,27 +157,38 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler();
+    app.UseForwardedHeaders();
 }
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(setupAction => // Filter Api Endpoint based on API Version in Swagger Docs
+    {
+        var descriptions = app.DescribeApiVersions();
+        foreach (var description in descriptions)
+        {
+            setupAction.SwaggerEndpoint(
+                $"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
-//app.MapControllers();
+app.UseCors("NgOrigins");
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-
 
 app.Run();
