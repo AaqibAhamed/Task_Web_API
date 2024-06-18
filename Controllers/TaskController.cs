12210348a8d744cc1da004f722ddc7bf2be6efc9
Aaqib-Innov8_Fusion <aaqib.innov8fusion@gmail.com>
2024-06-18 18:39:10 +0530

    Add Authorize, ApiVersion  and XML summary

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,154 +1,198 @@
+using Asp.Versioning;
 using AutoMapper;
 using Azure;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Task_Web_API.Models;
 
-[ApiController]
-[Route("api/tasks")]
-public class TaskController : ControllerBase
+namespace Task_Web_API.Controllers
 {
-    //private readonly TaskDbContext _context;
-    private readonly IMapper _mapper;
-    private readonly ITaskRepository _taskRepository;
-    private readonly ILogger<TaskController> _logger;
+    [ApiController]
+    [Route("api/v{version:apiVersion}/tasks")]
+    [Authorize]
+    //[Authorize(policy: "MustBeWiki")]  // - IF you uncomment it use userName as "wiki"
+    [ApiVersion(1)]
+    [ApiVersion(2)]
 
-    public TaskController(IMapper mapper, ITaskRepository taskRepository, ILogger<TaskController> logger)
+    public class TaskController : ControllerBase
     {
-        //_context = context;
-        _mapper  = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        _taskRepository = taskRepository ?? throw new ArgumentNullException(nameof(taskRepository));
-        _logger = logger ??  throw new ArgumentNullException(nameof(logger));
-    }
+        private readonly IMapper _mapper;
+        private readonly ITaskRepository _taskRepository;
+        private readonly ILogger<TaskController> _logger;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Task>>> GetAllTasks()
-    {
-        var tasks = await _taskRepository.GetAllTasksAsync();
-
-        if(tasks == null)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mapper"></param>
+        /// <param name="taskRepository"></param>
+        /// <param name="logger"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public TaskController(IMapper mapper, ITaskRepository taskRepository, ILogger<TaskController> logger)
         {
-            return NotFound();
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _taskRepository = taskRepository ?? throw new ArgumentNullException(nameof(taskRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-        
-        return Ok(_mapper.Map<IEnumerable<TaskDto>>(tasks));
-    }
 
-    [HttpGet("{id}", Name ="GetTask")]
-    public async Task<ActionResult<Task>> GetTask(int id)
-    {
-        /* if(!await _taskRepository.TaskExistsAsync(id))
+        /// <summary>
+        /// Get All Tasks
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Task>>> GetAllTasks()
         {
-            return NotFound();
-        } */
-        var task = await _taskRepository.GetTaskAsync(id);
+            var tasks = await _taskRepository.GetAllTasksAsync();
 
-        if (task == null)
-        {
-            return NotFound(new ProblemDetails
+            if (tasks == null)
             {
-                Status = 404,
-                Title = "Task not found",
-                Detail = "The specified task was not found in the database."
-            });
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<TaskDto>>(tasks));
         }
 
-        return Ok(_mapper.Map<TaskDto>(task));
+        /// <summary>
+        /// Get a Task by Id
+        /// </summary>
+        /// <param name="id">The Id of the Task to get </param>
+        /// <returns> A Task with Details</returns>
+        [HttpGet("{id}", Name = "GetTask")]
+        public async Task<ActionResult<Task>> GetTask(int id)
+        {
+
+            var task = await _taskRepository.GetTaskAsync(id);
+
+            if (task == null)
+            {
+                return NotFound(new ProblemDetails
+                {
+                    Status = 404,
+                    Title = "Task not found",
+                    Detail = "The specified task was not found in the database."
+                });
+            }
+
+            return Ok(_mapper.Map<TaskDto>(task));
+        }
+
+        /// <summary>
+        /// Create a Task
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<TaskDto>> CreateTask(TaskCreateDto task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var taskToCreate = _mapper.Map<Task>(task);
+
+            _taskRepository.AddTask(taskToCreate);
+
+            await _taskRepository.SaveChangesAsync();
+
+            var createdTaskToReturn = _mapper.Map<TaskDto>(taskToCreate);
+
+            return CreatedAtAction(nameof(GetTask), new { id = createdTaskToReturn.Id }, createdTaskToReturn);
+        }
+
+        /// <summary>
+        /// Update a Task
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="taskToUpdate"></param>
+        /// <returns></returns>
+        [HttpPut("{id}", Name = "UpdateTask")]
+        public async Task<ActionResult> UpdateTask(int id, TaskUpdateDto taskToUpdate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var taskEntity = await _taskRepository.GetTaskAsync(id);
+
+            if (taskEntity == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(taskToUpdate, taskEntity);
+
+            await _taskRepository.SaveChangesAsync();
+
+            return Ok(taskEntity); // return NoContent(); not looks nice
+
+        }
+
+        /// <summary>
+        /// Partially Update a Task with patchDocument
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="patchDocument"></param>
+        /// <returns></returns>
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> PartiallyUpdateTask(int id, JsonPatchDocument<TaskUpdateDto> patchDocument)
+        {
+            var taskEntity = await _taskRepository.GetTaskAsync(id);
+
+            if (taskEntity == null)
+            {
+                return NotFound();
+            }
+
+            var taskToPatch = _mapper.Map<TaskUpdateDto>(taskEntity);
+
+            patchDocument.ApplyTo(taskToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!TryValidateModel(taskToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+
+            _mapper.Map(taskToPatch, taskEntity);
+
+            await _taskRepository.SaveChangesAsync();
+
+            return Ok(taskEntity); // NoContent();
+        }
+
+        /// <summary>
+        /// Delete Task
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTask(int id)
+        {
+            /*   if(! await _taskRepository.TaskExistsAsync(id))
+              {
+                  return NotFound();
+              } */
+            var taskEntity = await _taskRepository.GetTaskAsync(id);
+
+            if (taskEntity == null)
+            {
+                return NotFound();
+            }
+
+            _taskRepository.DeleteTask(taskEntity);
+
+            await _taskRepository.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
     }
-
-    [HttpPost]
-    public async Task<ActionResult<TaskDto>> CreateTask(TaskCreateDto task)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        var taskToCreate = _mapper.Map<Task>(task);
-
-        _taskRepository.AddTask(taskToCreate);
-
-        await _taskRepository.SaveChangesAsync();
-
-        var createdTaskToReturn = _mapper.Map<TaskDto>(taskToCreate);
-
-        return CreatedAtAction(nameof(GetTask), new { id = createdTaskToReturn.Id }, createdTaskToReturn);
-    }
-
-    [HttpPut("{id}", Name ="UpdateTask")]
-    public async Task<ActionResult> UpdateTask(int id, TaskUpdateDto taskToUpdate)
-    {
-         if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        var taskEntity = await _taskRepository.GetTaskAsync(id);
-
-        if (taskEntity == null)
-        {
-            return NotFound();
-        }
-
-        _mapper.Map(taskToUpdate,taskEntity);
-
-        await _taskRepository.SaveChangesAsync();
-
-        return Ok(taskEntity); // return NoContent(); not looks nice
-
-    }
-
-    [HttpPatch("{id}")]
-    public async Task<ActionResult> PartiallyUpdateTask(int id, JsonPatchDocument<TaskUpdateDto> patchDocument)
-    {
-        var taskEntity = await _taskRepository.GetTaskAsync(id);
-
-        if(taskEntity == null)
-        {
-            return NotFound();
-        }
-
-        var taskToPatch = _mapper.Map<TaskUpdateDto>(taskEntity);
-
-        patchDocument.ApplyTo(taskToPatch, ModelState);
-
-        if(!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        if(!TryValidateModel(taskToPatch))
-        {
-            return BadRequest(ModelState);
-        }
-
-        _mapper.Map(taskToPatch,taskEntity);
-
-        await _taskRepository.SaveChangesAsync();
-
-        return Ok(taskEntity); // NoContent();
-    }
-
-     [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteTask(int id)
-    {
-      /*   if(! await _taskRepository.TaskExistsAsync(id))
-        {
-            return NotFound();
-        } */
-        var taskEntity = await _taskRepository.GetTaskAsync(id);
-
-        if(taskEntity == null)
-        {
-            return NotFound();
-        }
-
-        _taskRepository.DeleteTask(taskEntity);
-
-        await _taskRepository.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-
 }
