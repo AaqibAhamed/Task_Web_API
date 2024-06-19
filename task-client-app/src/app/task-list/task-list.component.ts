ea7e4bef236de255f9caec862cb3d78035a86463
Aaqib-Innov8_Fusion <aaqib.innov8fusion@gmail.com>
2024-06-19 19:16:03 +0530

    Add TaskListComponent

--- task-client-app/src/app/task-list/task-list.component.ts
+++ task-client-app/src/app/task-list/task-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
+export class TaskListComponent  implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
+
+  deleteTask(task: Task): void {
+    this.taskService.deleteTask(task.id).subscribe(() => this.getTasks());
+  }
 }
