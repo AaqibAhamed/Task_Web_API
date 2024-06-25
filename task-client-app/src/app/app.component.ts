02de9c73e01280612917ed301576596f926598fe
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-06-25 21:44:57 +0530

    Add Client App

--- task-client-app/src/app/app.component.ts
+++ task-client-app/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { NgFor } from '@angular/common';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 
-interface WeatherForecast {
+/* interface WeatherForecast {
   date: string;
   temperatureC: number;
   temperatureF: number;
@@ -21,76 +20,79 @@ interface Task {
   description: string;
   isCompleted: boolean;
 
-}
+} */
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppComponent, NgFor],
+  imports: [ AppComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
 
-  public forecasts: WeatherForecast[] = [];
+  title = 'Task Management App';
+  private readonly router: any
 
-  public taskData: Task[] = [];
-
-  public userData: User[] = [];
-
-
-  constructor(private http: HttpClient) {
-
-  }
-  ngOnInit() {
-
-    this.authenticate();
+  constructor(private authenticationService: AuthenticationService) {
 
   }
 
+  ngOnInit(): void {
+    if (!this.authenticationService.isAuthenticated()) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  /* public forecasts: WeatherForecast[] = [];
  
+ public taskData: Task[] = [];
+ 
+ public userData: User[] = []; */
 
-  title = 'task-client-app';
+
+  /* title = 'task-client-app';
   token = "";
   url ="https://localhost:7138/api/v1/"
  
   authenticate() {
     let endPoint = "authentication/authenticate";
-
+ 
     let body = {
       "username": "AaqibWiki",
       "password": "This is a relatively long sentence that acts as my password"
     }
-
+ 
     this.http.post(this.url + endPoint, body, { responseType: 'text' }).subscribe(
-
+ 
       (res) => {
-
+ 
         this.token = res;
-
+ 
         console.log(this.token, " res token");
-
+ 
         this.getTasks(this.token);
-
-
+ 
+ 
       },
       (error) => {
         console.error(error);
       }
     );
   }
-
+ 
   getTasks(tok:any) {
     let endPoint = "tasks";
-
+ 
     console.log(tok, " token");
-
+ 
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': "Bearer " + tok
     });
     let options = { headers: headers };
-
+ 
     this.http.get<Task[]>(this.url + endPoint, options).subscribe(
       (result) => {
         this.taskData = result;
@@ -101,17 +103,7 @@ export class AppComponent implements OnInit {
       }
     );
   }
-
-  selectTask(task: Task) {
-
-  }
-
-  deleteTask(task: Task) {
-
-  }
-
-  toggleTaskCompletion(task: Task) {
-
-  }
+ 
+  */
 
 }
--- task-client-app/src/app/app.routes.ts
+++ task-client-app/src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+//export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+      path: '',
+      component: TaskListComponent
+    },
+    {
+      path: 'tasks',
+      component: TaskListComponent
+    },
+    {
+      path: 'tasks/new',
+      component: TaskFormComponent
+    },
+    {
+      path: 'tasks/:id',
+      component: TaskFormComponent
+    },
+    {
+      path: 'login',
+      component: LoginComponent
+    }
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
