90ec4b8b0d103a84d3744ed22cdba0dc51716c45
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-06-25 22:15:48 +0530

    TaskListComponent

--- task-client-app/src/app/task-list/task-list.component.ts
+++ task-client-app/src/app/task-list/task-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../services/task.service';
 import { Task } from '../models/task.model';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
@@ -24,5 +26,20 @@ export class TaskListComponent  implements OnInit {
 
   deleteTask(task: Task): void {
     this.taskService.deleteTask(task.id).subscribe(() => this.getTasks());
+  }
+
+  selectTask(task: Task) {
+ 
+  }
+
+  saveTask(event:Event){
+
+  }
+
+  
+
+ 
+  toggleTaskCompletion(task: Task) {
+ 
   }
 }
