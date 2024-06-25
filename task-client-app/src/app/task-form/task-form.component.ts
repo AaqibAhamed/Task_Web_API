c10a149b6bbfa1d791e55eb470499d9e507a97e4
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-06-25 22:15:29 +0530

    Add TaskFormComponent

--- task-client-app/src/app/task-form/task-form.component.ts
+++ task-client-app/src/app/task-form/task-form.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
 
+  task: Task = new Task;
+
+  constructor(private taskService: TaskService) { }
+
+  saveTask(): void {
+    if (this.task.id) {
+      this.taskService.updateTask(this.task).subscribe(() => this.task);
+    } else {
+      this.taskService.createTask(this.task).subscribe(() => this.task);
+    }
+  }
 }
