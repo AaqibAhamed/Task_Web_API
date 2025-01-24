8903f5b5f53965b6794b66d1e17771bc487979e9
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-24 19:18:04 +0530

    Rename Tasks table to ToDoItems and update related migration files

---
+++ Migrations/20250124134554_Rename DbSet to ToDoItems.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Task_Web_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDbSettoToDoItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.CreateTable(
+                name: "ToDoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDoItems",
+                columns: new[] { "Id", "Description", "IsCompleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, "moring asdad", true, "Wake Up" },
+                    { 2, "go washroom", false, "Answer the Nature" },
+                    { 3, "eat healthy food", false, "Take Breakfast" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItems");
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "Description", "IsCompleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, "moring asdad", true, "Wake Up" },
+                    { 2, "go washroom", false, "Answer the Nature" },
+                    { 3, "eat healthy food", false, "Take Breakfast" }
+                });
+        }
+    }
+}
