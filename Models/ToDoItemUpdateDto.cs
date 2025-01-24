f83f52b0da521282ac652bce5b9e269b6add9527
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-24 23:53:59 +0530

    Add ToDoItemUpdateDto model for task updates

---
+++ Models/ToDoItemUpdateDto.cs
@@ -0,0 +1,31 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Task_Web_API.Models
+{
+
+    /// <summary>
+    /// Task Update Dto
+    /// </summary>
+    public class ToDoItemUpdateDto
+    {
+        /// <summary>
+        ///  The Title of the Task 
+        /// </summary>
+        [Required(ErrorMessage = "You should provide a Title value.")]
+        [MaxLength(50)]
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  The description of the Task
+        /// </summary>
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        /// <summary>
+        ///  Is Task completed or not 
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+    }
+}
