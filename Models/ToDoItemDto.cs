8a187de22f203a65b7e7d7d5a782986a649fc8fa
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-25 11:55:03 +0530

    Refactor ToDoItemDto to use Guid for Id and add CreatedAt and CompletedAt properties

--- Models/ToDoItemDto.cs
+++ Models/ToDoItemDto.cs
@@ -1,29 +1,33 @@
-
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-/// <summary>
-/// Task Get Dto
-/// </summary>
-public class ToDoItemDto
+namespace Task_Web_API.Models
 {
-    /// <summary>
-    ///  The Id of the Task 
-    /// </summary>
-    public int Id { get; set; }
 
     /// <summary>
-    ///  The Title of the Task 
+    /// Task Get Dto
     /// </summary>
-    public string Title { get; set; } = string.Empty;
+    public class ToDoItemDto
+    {
+        /// <summary>
+        ///  The Id of the Task 
+        /// </summary>
+        public Guid Id { get; set; }
 
-    /// <summary>
-    /// The description of the Task
-    /// </summary>
-    public string? Description { get; set; }
+        /// <summary>
+        ///  The Title of the Task 
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
 
-    /// <summary>
-    ///   Is Task completed or not 
-    /// </summary>
-    public bool IsCompleted { get; set; }
+        /// <summary>
+        /// The description of the Task
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        ///   Is Task completed or not 
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+    }
 }
