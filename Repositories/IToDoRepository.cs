046b9491411fd1f57557e6ba8382305af6519146
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-25 06:41:49 +0530

    Replace ITaskRepository with IToDoRepository interface

---
+++ Repositories/IToDoRepository.cs
@@ -0,0 +1,23 @@
+namespace Task_Web_API.Repositories
+{
+    public interface IToDoRepository
+    {
+        Task<IEnumerable<ToDoItem>> GetAllTasksAsync();
+
+        Task<ToDoItem?> GetTaskAsync(int taskId);
+
+        Task<IEnumerable<ToDoItem>> GetAllCompletedTasksAsync(bool? IsCompleted);
+
+        Task<IEnumerable<ToDoItem>> GetAllPendingTasksAsync(bool? IsCompleted);
+
+        Task<bool> TaskExistsAsync(int taskId);
+
+        void AddTask(ToDoItem task);
+
+        void DeleteTask(ToDoItem task);
+
+        Task<bool> SaveChangesAsync();
+
+    }
+
+}
