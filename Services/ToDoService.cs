3dac1355195d5bc1d4ce34f5744fad534d6078d2
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-25 10:17:50 +0530

    Replace TaskService with ToDoService implementation

---
+++ Services/ToDoService.cs
@@ -0,0 +1,47 @@
+namespace Task_Web_API.Services
+{
+    public class ToDoService : IToDoService
+    {
+        public void AddTask(ToDoItem task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTask(ToDoItem task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoItem>> GetAllCompletedTasksAsync(bool? IsCompleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoItem>> GetAllPendingTasksAsync(bool? IsCompleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoItem>> GetAllTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoItem?> GetTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TaskExistsAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
