16576d0fbc0bf3bf2bccd2109cd456023415cd34
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-27 01:42:32 +0530

    Refactor IToDoService interface to return ResponseDto and update method signatures for task management

--- Services/IToDoService.cs
+++ Services/IToDoService.cs
@@ -1,22 +1,25 @@
 using Task_Web_API.Entities;
+using Task_Web_API.Models;
 
 namespace Task_Web_API.Services
 {
     public interface IToDoService
     {
-        Task<IEnumerable<ToDoItem>> GetAllTasksAsync();
+        Task<ResponseDto> GetAllTasksAsync();
 
-        Task<ToDoItem?> GetTaskAsync(int taskId);
+        Task<ToDoItemDto> GetTaskByIdAsync(Guid taskId);
+
+        Task<ResponseDto> CreateTaskAsync(ToDoItemCreateDto toDoItemCreateDto );
+
+        Task<ResponseDto> EditTaskAsync(Guid taskId, ToDoItemUpdateDto toDoItemUpdateDto);
+
+        Task<ResponseDto> DeleteTaskAsync(Guid id);
 
         Task<IEnumerable<ToDoItem>> GetAllCompletedTasksAsync(bool? IsCompleted);
 
         Task<IEnumerable<ToDoItem>> GetAllPendingTasksAsync(bool? IsCompleted);
 
-        Task<bool> TaskExistsAsync(int taskId);
-
-        void AddTask(ToDoItem task);
-
-        void DeleteTask(ToDoItem task);
+        Task<bool> TaskExistsAsync(Guid taskId);
 
         Task<bool> SaveChangesAsync();
 
